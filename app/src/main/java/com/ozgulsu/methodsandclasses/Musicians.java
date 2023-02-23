1c9f11e9ea5a5731991c427b03008522bdf45f85
gulsuoz <gulsuoz0296@gmail.com>
2023-02-23 23:56:06 +0300

    methodsandclasses
    methods and classes

---
+++ app/src/main/java/com/ozgulsu/methodsandclasses/Musicians.java
@@ -0,0 +1,19 @@
+package com.ozgulsu.methodsandclasses;
+
+public class Musicians {
+    //attribute
+    String name;
+    String instrument;
+    int age;
+
+    public Musicians(String name, String instrument, int age) {
+        this.name = name;
+        this.instrument = instrument;
+        this.age = age;
+        System.out.println("constructor called");
+    }
+
+
+
+
+}
